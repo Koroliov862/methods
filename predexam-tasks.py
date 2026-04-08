27a072bed866c4f4cc91e65ecdc3c327c0472d61
Koroliov862 <dimakorolev35939@gmail.com>
2026-04-08 09:36:31 +0500

    podv

--- predexam-tasks.py
+++ predexam-tasks.py
@@ -1,10 +1,19 @@
 import numpy as np
-from scipy.interpolate import lagrange, KroghInterpolator, quad, dblquad, tplquad, nquad
-
+from scipy.interpolate import lagrange, KroghInterpolator, CubicSpline
+import matplotlib.pyplot as plt
 #1
 #=======================================================================================
 
-temps = np.array([15, 18, 22, 20])
-times = np.array([8, 10, 13, 16])
+x = np.array([15, 18, 22, 20])
+y = np.array([8, 10, 13, 16])
 
-newton_inrep = KroghInterpolator(times, temps)
+newton_poly = KroghInterpolator(x, y)
+time = np.array([12, 14, 15.5])
+for i in time:
+    print(f"Температура в {i}: {newton_poly(i)}")
+
+plt.plot(time, newton_poly(time), 'b-', label = "Ньютон")
+plt.plot(x, y, 'ro', label ="Исходные Точки")
+plt.legend()
+plt.grid(True)
+plt.show()
